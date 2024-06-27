c484899ad89c8985410b429676421941ac94963c
Evil2997 <dima.molchanow2012@gmail.com>
2024-06-27 17:29:09 +0300

    Add more outputs in terminal, output.json printed in terminal

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
 # Get the current time formatted as "YYYY.MM.DD-HH.MM"
 current_time=$(date "+%Y.%m.%d-%H.%M")
 
 # Set root directory based on the upload variable
 if [ -z "$upload" ]; then
-    root_dir=/shared/
+    root_dir="/shared/"
 else
-    root_dir=/
+    root_dir="/"
     mkdir -p /xml_files
     mkdir -p /reports
 fi
@@ -38,6 +38,12 @@ get_filename() {
 # Create necessary directories
 mkdir -p "$root_dir$xml_dir"
 
+# Check if the input file exists
+if [ ! -f shared/ips.txt ]; then
+    echo "shared/ips.txt not found."
+    exit 1
+fi
+
 # Read lines from the input file and process each IP
 while IFS= read -r line; do
     current_time=$(date "+%Y.%m.%d-%H.%M.%S")
@@ -51,6 +57,10 @@ python /output_report.py "$root_dir$xml_dir" "$root_dir$report_file" /shared/ips
 
 # Apply text replacements for LaTeX if the report extension is "tex"
 if [ "$report_extension" = "tex" ]; then
+    if [ ! -f "$root_dir$report_file" ]; then
+        echo "$root_dir$report_file not found."
+        exit 1
+    fi
     sed -i 's/_/\\_/g' "$root_dir$report_file"
     sed -i 's/\$/\\\$/g' "$root_dir$report_file"
     sed -i 's/#/\\#/g' "$root_dir$report_file"
@@ -59,3 +69,11 @@ fi
 
 # Upload the final report file
 upload "$report_file"
+
+# output.json
+echo "========================================"
+if [ -f "$root_dir$report_file" ]; then
+    cat "$root_dir$report_file"
+else
+    echo "$root_dir$report_file not found."
+fi
